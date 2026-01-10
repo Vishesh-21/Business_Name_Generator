a70472ab26148aaec1c5df25cf1ed10a054f8c5c
Vishu Verma <visheshbaund@gmail.com>
2026-01-10 12:32:06 +0530

    Refactor BottomBar component to use dynamic card rendering and add Textarea component

--- components/BottomBar.jsx
+++ components/BottomBar.jsx
@@ -1,41 +1,59 @@
 import React from "react";
 import { Filter, Heart, Lightbulb } from "lucide-react";
 
+const data = [
+  {
+    title: "Generate Idea",
+    description:
+      "Tap to instantly generate unique and creative business name ideas tailored to your needs!",
+    icon: <Lightbulb className="text-primary" size={35} />,
+  },
+  {
+    title: "Filter Result",
+    description:
+      "Refine your results by applying filters like industry, style, or keywords to find the perfect match.",
+    icon: <Filter className="text-primary" size={35} />,
+  },
+  {
+    title: "Save Name",
+    description:
+      "Found a name you love? Save it for later and keep all your favorites in one place!",
+    icon: <Heart className="text-primary" size={35} />,
+  },
+];
+
 export const BottomBar = () => {
   return (
     <>
       <div className="flex flex-col px-10 md:px-0 md:flex-row justify-between md:gap-10 gap-4 mt-12 text-white">
-        <div className="flex flex-col gap-2 md:items-center px-4 py-4 rounded-md hover:bg-gray-800 transition-all duration-200">
-          <Lightbulb className="text-primary text-3xl" size={35} />
-          <h1 className="text-xl font-semibold">Generate Idea</h1>
-          <p className="md:text-center text-left">
-            Tap to instantly generate unique and creative business name ideas
-            tailored to your needs!
-          </p>
-        </div>
-        <div className="flex flex-col gap-2 md:items-center px-4 py-4 rounded-md hover:bg-gray-800 transition-all duration-200">
-          <Filter className="text-primary text-3xl" size={35} />
-          <h1 className="text-xl font-semibold">Filter Result</h1>
-          <p className="md:text-center text-left">
-            Refine your results by applying filters like industry, style, or
-            keywords to find the perfect match.
-          </p>
-        </div>
-        <div className="flex flex-col gap-2 md:items-center px-4 py-4 rounded-md hover:bg-gray-800 transition-all duration-200">
-          <Heart className="text-primary text-3xl" size={35} />
-          <h1 className="text-xl font-semibold">Save Name</h1>
-          <p className="md:text-center text-left">
-            Found a name you love? Save it for later and keep all your favorites
-            in one place!
-          </p>
-        </div>
+        {/* cards  */}
+        {data.map((item, index) => (
+          <Card
+            key={index}
+            title={item.title}
+            description={item.description}
+            icon={item.icon}
+          />
+        ))}
       </div>
       <div className="w-full md:fixed bottom-0">
         <hr className="md:w-[100%] mt-5 mx-auto" />
         <footer className="text-white text-center text-gray-500">
-          <h1 className="mt-3">&copy; {new Date().getFullYear()}{" "} | All rights are reserved.</h1>
+          <h1 className="mt-3">
+            &copy; {new Date().getFullYear()} | All rights are reserved.
+          </h1>
         </footer>
       </div>
     </>
   );
 };
+
+function Card({ title, description, icon }) {
+  return (
+    <div className="flex flex-col gap-2 md:items-center px-4 py-4 rounded-md border-[1px]  bg-primary/20 border-primary transition-all duration-300">
+      {icon}
+      <h1 className="font-semibold text-medium">{title}</h1>
+      <p className="md:text-center text-left text-base">{description}</p>
+    </div>
+  );
+}
